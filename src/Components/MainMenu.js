7b9b250a9f50c85fc9c36a0635e83ddf9bef6567
Bishal <btthapa82@gmail.com>
2023-02-28 19:30:11 +0545

    Adding comment on MainMenu

--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -1,13 +1,19 @@
+// Import the CSS file for styling
 import "../App.css";
+
 import React, { useContext } from "react";
 import { QuizContext } from "../Helpers/Context";
 
-
+// Define a functional component for the main menu of the quiz
 export default function MainMenu() {
+  // Access the quiz context to retrieve the setGameState function
   const { setGameState } = useContext(QuizContext);
+
+  // Render the main menu of the quiz, consisting of a single button to start the quiz
   return (
     <div className="Menu">
       <button
+        // When the button is clicked, call the setGameState function to change the game state to "quiz"
         onClick={() => {
           setGameState("quiz");
         }}
@@ -16,4 +22,4 @@ export default function MainMenu() {
       </button>
     </div>
   );
-}
+};
